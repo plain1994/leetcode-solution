ef6a598506ea975a0c1665388dcafcb795613afa
tom <plain1994@gmail.com>
2016-08-07 22:13:57 +0800

    107 binary tree level order traversal 2

--- python/107_Binary_Tree_Level_Order_Traversal_II.py
+++ python/107_Binary_Tree_Level_Order_Traversal_II.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"102. Binary Tree Level Order Traversal"
+"107. Binary Tree Level Order Traversal II"
 __author__ = 'Tom Gou'
 
 """
-Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
 
 For example:
 Given binary tree [3,9,20,null,null,15,7],
@@ -14,20 +14,14 @@ Given binary tree [3,9,20,null,null,15,7],
   9  20
     /  \
    15   7
-return its level order traversal as:
+return its bottom-up level order traversal as:
 [
-  [3],
+  [15,7],
   [9,20],
-  [15,7]
+  [3]
 ]
 
-这道题要求二叉树的层次遍历，可以通过前序遍历加记录来实现。
-没遍历一个节点，就把该节点放到对应的数组里。
-数组个数可以提前计算二叉树深度来判断，也可以加入判断，
-在数组个数不够时加入一个新数组。
-
-解法1用到的前序遍历是递归实现的。
-
+较为简单的想法是对层次遍历的结果进行翻转。如102 binary tree level order traversal
 """
 
 # Definition for a binary tree node.
@@ -45,6 +39,7 @@ class Solution(object):
         """
         res = []
         self.preOrder(root, 0, res)
+        res.reverse()#res[::-1]
         return res
 
     def preOrder(self, root, level, res):
